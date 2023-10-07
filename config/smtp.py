90d890d2a82d421b96ca6ac5c928534ccd730749
xiaotiancaipro <xiaotiancaipro@163.com>
2023-10-07 15:36:32 +0800

    Optimize SMTP configuration.

--- config/smtp.py
+++ config/smtp.py
@@ -7,29 +7,24 @@ class SMTPConfig(Config):
     """配置 SMTP 服务器参数"""
 
     def __init__(self):
-
-        self.__host = os.getenv("SMTP_HOST")
-        self.__port = os.getenv("SMTP_PORT")
-        self.__user = os.getenv("SMTP_USER")
-        self.__password = os.getenv("SMTP_PASSWORD")
-
-        if not self.__host:
-            raise Exception("SMTP 服务器参数 host 为空")
-        if not self.__port:
-            raise Exception("SMTP 服务器参数 port 为空")
-        if not self.__user:
-            raise Exception("SMTP 服务器参数 user 为空")
-        if not self.__password:
-            raise Exception("SMTP 服务器参数 password 为空")
+        self.__configuration_dict = {
+            "host": os.getenv("SMTP_HOST"),
+            "port": os.getenv("SMTP_PORT"),
+            "user": os.getenv("SMTP_USER"),
+            "password": os.getenv("SMTP_PASSWORD")
+        }
+        for key in self.__configuration_dict.keys():
+            if self.__configuration_dict[key] == "":
+                raise Exception(f"SMTP 服务器参数 {key} 为空")
 
     def host(self):
-        return self.__host
+        return self.__configuration_dict["host"]
 
     def port(self):
-        return int(self.__port)
+        return int(self.__configuration_dict["port"])
 
     def user(self):
-        return self.__user
+        return self.__configuration_dict["user"]
 
     def password(self):
-        return self.__password
+        return self.__configuration_dict["password"]
