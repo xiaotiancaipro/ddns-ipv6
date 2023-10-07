c90b9964ed1351064af0092c85c83feb0fde9d43
xiaotiancaipro <xiaotiancaipro@163.com>
2023-10-07 15:19:52 +0800

    Optimize Email configuration.

--- config/email.py
+++ config/email.py
@@ -7,11 +7,13 @@ class EmailConfig(Config):
     """配置邮件参数"""
 
     def __init__(self):
+
         self.__sender = os.getenv("EMAIL_SENDER"),
         self.__receivers = [os.getenv("EMAIL_RECEIVERS")]
-        if self.__sender is None:
+
+        if not self.__sender:
             raise Exception("发件人配置信息为空")
-        if self.__receivers is None:
+        if not self.__receivers:
             raise Exception("收件人配置信息为空")
 
     def sender(self):
