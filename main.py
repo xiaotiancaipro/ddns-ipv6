37a5b38d7200da11beecbc1e36e1d9bf133eee21
xiaotiancaipro <xiaotiancaipro@163.com>
2023-10-07 14:48:36 +0800

    Optimize main.

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-from view import ipv6_to_email
+import sys
+
+from view import ipv6_to_email, ipv6_to_email_anyway
+
+
+def main():
+    if len(sys.argv) == 1:
+        ipv6_to_email()
+        return
+    if sys.argv[1] == "boot":
+        ipv6_to_email_anyway()
+        return
+    print("--> 输入参数有误 <--")
+
 
 if __name__ == '__main__':
-    ipv6_to_email()
+    main()
--- main_boot.py
+++
@@ -1,4 +0,0 @@
-from view import ipv6_to_email_anyway
-
-if __name__ == '__main__':
-    ipv6_to_email_anyway()
