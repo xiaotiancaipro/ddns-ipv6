dd133ac1200cccffe3bcd927031935db953a3cc1
xiaotiancaipro <xiaotiancaipro@163.com>
2024-01-18 10:35:13 +0800

    feat: add redis configuration

--- api/config.py
+++ api/config.py
@@ -16,11 +16,21 @@ class Config(object):
 
     SYSTEM_SECRET_KEY = get_env("SYSTEM_SECRET_KEY")
 
-    SQLALCHEMY_DATABASE_URI = "sqlite://data/ipv6.db"
+    # Redis
+    REDIS_PASSWORD = get_env("REDIS_PASSWORD")
+    REDIS_HOST = get_env("REDIS_HOST")
+    REDIS_PORT = get_env("REDIS_PORT")
+    REDIS_URL = f"redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/0"
+    CELERY_BROKER_URL = f"redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/1"
+    CELERY_BACKEND_URL = f"redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/2"
+
+    # Database
+    SQLALCHEMY_DATABASE_URI = "sqlite:///data/ipv6.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_ENGINE_OPTIONS = {"pool_size": 30, "pool_recycle": 3600}
 
+    # Email Server
     SMTP_HOST = get_env("SMTP_HOST")
     SMTP_PORT = get_env("SMTP_PORT")
     SMTP_USER = get_env("SMTP_USER")
