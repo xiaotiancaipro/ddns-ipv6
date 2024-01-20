06c1de3b901fc74478f5d370727c6b14efb5cd0a
xiaotiancaipro <xiaotiancaipro@163.com>
2024-01-20 15:28:22 +0800

    feat: change database from sqlite to postgresql

--- api/config.py
+++ api/config.py
@@ -25,7 +25,18 @@ class Config(object):
     CELERY_BACKEND_URL = f"redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}/2"
 
     # Database
-    SQLALCHEMY_DATABASE_URI = "sqlite:///data/ipv6.db"
+    DB_USERNAME = get_env("DB_USERNAME")
+    DB_PASSWORD = get_env("DB_PASSWORD")
+    DB_HOST = get_env("DB_HOST")
+    DB_PORT = get_env("DB_PORT")
+    DB_DATABASE = get_env("DB_DATABASE")
+    SQLALCHEMY_DATABASE_URI = "postgresql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}".format(
+        USERNAME=DB_USERNAME,
+        PASSWORD=DB_PASSWORD,
+        HOST=DB_HOST,
+        PORT=DB_PORT,
+        DATABASE=DB_DATABASE
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_ENGINE_OPTIONS = {"pool_size": 30, "pool_recycle": 3600}
