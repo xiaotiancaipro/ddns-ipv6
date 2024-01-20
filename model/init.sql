92924d48f8c290e65ec23953bc420ae269dd32cb
xiaotiancaipro <xiaotiancaipro@163.com>
2024-01-20 17:09:24 +0800

    Initialize database

--- model/init.sql
+++ model/init.sql
@@ -1,6 +1,6 @@
 CREATE TABLE network
 (
-    id         uuid      NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
+    id         BIGSERIAL PRIMARY KEY,
     ip_addr    text               DEFAULT NULL,
-    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
