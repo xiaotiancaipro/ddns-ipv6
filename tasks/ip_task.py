2de7fa09b5a11aca3cceeb2a2354ca2fa89bfaae
xiaotiancaipro <xiaotiancaipro@163.com>
2024-02-18 20:40:49 +0800

    perf: Optimize the order of inserting into the database, sending emails and domain name resolution when the IPv6 address changes

--- tasks/ip_task.py
+++ tasks/ip_task.py
@@ -23,16 +23,19 @@ def update_ipv6():
         return
 
     # If ipv6 address has changed, so insert it into the database, send an email and auto-ddns
-    NetworkOperation.insert(ipv6_address=ipv6_address)
-    EmailService.send_ipv6(ipv6_address=ipv6_address)
+    flag = EmailService.send_ipv6(ipv6_address=ipv6_address)
+    if not flag:
+        return
     if Config.DOMAIN_NAME and Config.RR and Config.ALIYUN_ACCESSKEY_ID and Config.ALIYUN_ACCESSKEY_SECRET:
-        aliyun_ddns = AliyunDDNS()
-        aliyun_ddns.upgrade_records(
+        flag = AliyunDDNS().upgrade_records(
             rr=Config.RR,
             value=ipv6_address,
             type="AAAA",
             ttl=Config.TTL if Config.TTL else 600
         )
+        if not flag:
+            return
+    NetworkOperation.insert(ipv6_address=ipv6_address)
 
     return
 
@@ -40,21 +43,9 @@ def update_ipv6():
 @shared_task
 def schedule_ipv6():
     """Get regularly IPv6 address task"""
-
-    # Get current and database addresses
-    ipv6_address, ipv6_address_db = IPService.get_ipv6_public(), IPService.get_ipv6_db()
+    ipv6_address = IPService.get_ipv6_public()  # Get current
     if ipv6_address is None:
         EmailService.send_ipv6_not_obtained()
         return
-    if ipv6_address_db is None:
-        EmailService.send_ipv6_db_not_obtained()
-        return
-
-    # Insert into database when the ipv6 address has changed
-    if ipv6_address != ipv6_address_db:
-        NetworkOperation.insert(ipv6_address=ipv6_address)
-
-    # Send an email
-    EmailService.send_ipv6(ipv6_address=ipv6_address)
-
+    EmailService.send_ipv6(ipv6_address=ipv6_address)  # Send an email
     return
