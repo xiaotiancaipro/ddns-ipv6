2a3e4f37e5641352fd5620499dd585a6a341968d
xiaotiancaipro <xiaotiancaipro@163.com>
2024-02-18 16:33:34 +0800

    style: rename

--- extensions/ext_celery_beats_prod.py
+++ extensions/ext_celery_beats_prod.py
@@ -2,11 +2,11 @@ from celery.schedules import crontab
 
 prod_beats = {
     "update_ipv6": {
-        "task": "tasks.network_task.update_ipv6",
+        "task": "tasks.ip_task.update_ipv6",
         "schedule": 300,
     },
     "schedule_ipv6": {
-        "task": "tasks.network_task.schedule_ipv6",
+        "task": "tasks.ip_task.schedule_ipv6",
         "schedule": crontab(minute="00", hour="09"),
     },
 }
